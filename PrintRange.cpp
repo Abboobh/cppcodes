292ddb98e357f68f8a749f891585f568476df828
_Vladimir <125105373+Abboobh@users.noreply.github.com>
2024-07-02 07:06:13 +0300

    Methods.cpp

--- PrintRange.cpp
+++ PrintRange.cpp
@@ -8,3 +8,23 @@ void PrintRange(T begin, T end)
     }
     cout << endl;
 }
+
+template<typename Container, typename Type>
+void FindAndPrint(Container value, Type var)
+{
+    auto range_begin = value.begin();
+    auto range_end = find_if(value.begin(), value.end(), [var](Type _type) {return _type == var; });
+
+    while (range_begin != range_end)
+    {
+        cout << *range_begin << " ";
+        range_begin++;
+    }
+    cout << endl;
+    while (range_end != value.end())
+    {
+        cout << *range_end << " ";
+        range_end++;
+    }
+    cout << endl;
+}
